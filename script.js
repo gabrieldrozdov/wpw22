8476592797cad0eea5f69c0ec8c55106803dfdaf
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2022-10-09 23:50:21 -0400

    removed sticker javascript code

--- script.js
+++ script.js
@@ -23,37 +23,6 @@ function setTime() {
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-/* Element sticker code adapted from Marie Otsuka's Web Type syllabus */
-$(function(){
-
-	$("main").on("click", function(event){
-
-		if ($(event.target).hasClass('element')){
-			$(event.target).remove();
-		} else{
-			var element = event.target.tagName;
-			var x = event.pageX;
-			var y = event.pageY;
-
-			// console.log(element);
-			var sticker = '<div class="element" id="added" style="text-transform:lowercase">&lt;'+ element +'&gt</div>';
-			$('main').append(sticker);
-			
-			$('#added').css({
-			  	 top:y-20, 
-			  	 left:x-20,
-			 }).removeAttr('id');
-		}
-	});
-
-	$(window).resize(function() {
-	 	$('.element').each(function(){
-	 		$(this).remove();
-	 	});
-	});
-
-});
-
 /* Up button rotation */
 window.onscroll = function () {
     scrollRotate();
